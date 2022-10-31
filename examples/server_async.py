aa5c8ef97adff3131ebb4bef499e2efddfee76a1
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-31 18:57:16 +0100

    Update server_async.py

--- examples/server_async.py
+++ examples/server_async.py
@@ -70,8 +70,8 @@ def setup_server(args):
         # Continuing, or use a sparse DataBlock which can have gaps
         
         # enter datablock here for BYD battery register
-        #datablock = ModbusSparseDataBlock({101: [0, 7], 40010: [0, 0, 0, 3, 1, 2, 0, 0, 0, 4]})
-        datablock = ModbusSparseDataBlock({101: [0, 7], 40010: [0, 0, 0, 0, 0, 0, 7, B, Y, D]})
+        #datablock = ModbusSparseDataBlock({101: [0, 7], 40010: [0, 0, 0, 2013, 1, 2, 0, 0, 0, 4]})
+        datablock = ModbusSparseDataBlock({101: [0, 7], 40010: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]})
         
     elif args.store == "factory":
         # Alternately, use the factory methods to initialize the DataBlocks
