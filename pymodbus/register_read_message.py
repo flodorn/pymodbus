2a159d544436120f938fb24b7a8bfd0bf78b2e52
flodorn <44878709+flodorn@users.noreply.github.com>
2022-11-01 19:52:47 +0100

    Update register_read_message.py

--- pymodbus/register_read_message.py
+++ pymodbus/register_read_message.py
@@ -83,7 +83,7 @@ class ReadRegistersResponseBase(ModbusResponse):
         _logger.debug("-----encode self")
         result = struct.pack(">B", len(self.registers) * 2)
         for register in self.registers:
-            result += struct.pack(">H", register)
+            result += struct.pack(">c", register)
         return result
 
     def decode(self, data):
